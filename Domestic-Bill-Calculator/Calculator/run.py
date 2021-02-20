b0786ee7a09c0fcee2881da2d6681b406df95038
deepunahak <deepunahak@gmail.com>
2021-02-20 14:15:40 +0530

    Updated ...

--- Domestic-Bill-Calculator/Calculator/run.py
+++ Domestic-Bill-Calculator/Calculator/run.py
@@ -14,13 +14,19 @@ def main():
         ops = input("Enter your choice: ")
         # switch(operation)
         if ops == '1':
-            print("-*-"*20)
-            house_sq_ft = int(input("Enter area detail in square feet to count bill for house: "))
-            water_liter = int(input("Enter water detail in liter to count water bill: "))
-            electricity_kwh = int(input("Enter Electricity detail in kilowatt hour(kwh) to count Electricity bill: "))
-            phone_seconds = int(input("Enter Phone duration in seconds to count phone bill: "))
-            gas_liter = int(input("Enter gas detail in liter to count gas bill: "))
-            print("-*-"*20)
+            print("-*-"*30)
+            print("Note: All value should be positive integer.")
+            print("-*-"*30)
+            try:
+                house_sq_ft = int(input("Enter area detail in square feet to count bill for house: "))
+                water_liter = int(input("Enter water detail in liter to count water bill: "))
+                electricity_kwh = int(input("Enter Electricity detail in kilowatt hour(kwh) to count Electricity bill: "))
+                phone_seconds = int(input("Enter Phone duration in seconds to count phone bill: "))
+                gas_liter = int(input("Enter gas detail in liter to count gas bill: "))
+            except ValueError as ve:
+                print("Please enter positive integer value !!!")
+                continue
+            print("-*-"*30)
             calc.calc_monthly_bill(house_sq_ft, water_liter, electricity_kwh, phone_seconds, gas_liter)
             calc.print_bill()
         elif ops == '2':
